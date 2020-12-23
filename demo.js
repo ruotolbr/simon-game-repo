1deeef29a418c0170781547b58430b57b0641288
ruotolbr1 <ruotolbr@gmail.com>
2020-12-23 09:01:40 -0500

    update

---
+++ demo.js
@@ -0,0 +1,12 @@
+//setInterval() for color sequences
+//ex: 
+let startColorSeq = 0;
+function startSequencer(){
+let listElements = [
+{id: "redBtn", timer 2000}
+{id: "yellowBtn", timer 2000}
+{id: "greenBtn", timer 2000} 
+{id: "blueBtn", timer 2000}     
+];
+    setInterval() 
+}
